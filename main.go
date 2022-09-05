3525f8e5522282fdf7a88ee0b818b3418c41efd8
Vanshika Kaul <vanshikakaul@192.168.1.4>
2022-09-05 11:10:32 +0530

    Trial check

--- main.go
+++ main.go
@@ -10,11 +10,9 @@ import (
 
 // checkNumInFile function checks whether the input number
 // is present in the file or not.
-func checkNumInFile(inputNum int, filename string) int {
+func checkNumInFile(inputNum int, filename string) (int, int) {
 
 	fmt.Println("Processing the file.....")
-
-	inputStr := strconv.Itoa(inputNum)
 
 	var data []byte
 	var err error
@@ -30,36 +28,45 @@ func checkNumInFile(inputNum int, filename string) int {
 	//into string array
 	strArr := strings.Split(s, ",")
 
-	// loop over the array and check whether the
-	//input is present.
-	for i := 0; i < len(strArr); i++ {
-
-		if strArr[i] == inputStr {
-			return i
-		}
-
+	//convert string slice into int slice
+	IntSlice := make([]int, len(strArr))
+	for i, stt := range strArr {
+		IntSlice[i], _ = strconv.Atoi(stt)
 	}
 
-	//return -1 if the input is not present
-	return -1
+	//sort map by key
+	m, _ := make(map[int]int), err
+	for i := 0; i < len(IntSlice); i++ {
+		m[IntSlice[i]] = i + 1
+	}
+	fmt.Println("Position of next highest elements:", m[inputNum+1])
+	//fmt.Println(inputNum)
 
+	n, ok := m[inputNum]
+	if ok == true {
+		return n, m[inputNum+1]
+	}
+	//return -1 if the input is not present
+	return -1, 0
 }
 
 func main() {
 
 	var inputNum int
-
 	fmt.Println("Enter number you want to find:")
-	fmt.Scanf("%d\n", &inputNum)
+	if _, err := fmt.Scan(&inputNum); err != nil {
+		log.Printf("Enter valid number ") //enter cases if number entered is not string.
+		return
+	}
 
 	filename := "./randomNumbers.txt"
+	result, nextVal := checkNumInFile(inputNum, filename)
 
-	result := checkNumInFile(inputNum, filename)
-
-	if result == -1 {
+	if result == -1 || result == 0 {
 		fmt.Println("The number is not present.")
 	} else {
-		fmt.Println("The number is present at the postion", result+1)
+		fmt.Println("Position of number:", result)
+		fmt.Println("Position of next highest number:", nextVal)
 	}
 
 }
